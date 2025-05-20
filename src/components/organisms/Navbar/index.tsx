8ade36289b9463da9587729c30424e9a92c335ed
mateo.velasquez <mateo.velasquez@ServiciosNutresa.onmicrosoft.com>
2025-05-19 19:57:40 -0500

    feat: nabvar ajustment

--- src/components/organisms/Navbar/index.tsx
+++ src/components/organisms/Navbar/index.tsx
@@ -3,7 +3,7 @@ import NavTitle from '../../atoms/NavTitle/index'
 
 export default function Navbar() {
   return (
-    <nav className='flex flex-col lg:flex-row items-center justify-around bg-secondary h-[90px] lg:h-[46px] w-full shadow-md mb-2'>
+    <nav className='flex flex-col lg:flex-row items-center justify-around bg-secondary h-[150px] lg:h-[46px] w-full shadow-md mb-2'>
         <div><NavTitle title='Eventos y Actividades' link='/EventosYactividades' /></div>
         <div><NavTitle title='Comunidades' link='/Comunidades' /></div>
         <div><NavTitle title='Valoraciones' link='/Valoraciones' /></div>
