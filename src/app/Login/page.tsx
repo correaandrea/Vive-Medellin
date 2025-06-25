a67f74a3b18108feed80db08ed66e524261c4010
Andrea Correa Arango <andrea.correaa1@udea.edu.co>
2025-06-25 11:25:44 -0500

    Revert "feat: Add E2E tests for registration and login with Cypress"
    B
    This reverts commit 00c95386ecd111a30eb1d8bc16b213bdabe85d16.

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,86 +1,80 @@
 'use client'
-
 import React, { useState } from 'react'
 import Footer from '../../components/atoms/Footer/index'
+import Icon from '../../components/atoms/Icon/index'
+import NavTitle from '../../components/atoms/NavTitle/index'
 import Text from '../../components/atoms/Text/index'
 import TextBox from '../../components/atoms/TextBox/index'
 import Button from '../../components/atoms/Button/index'
 
 export default function Login () {
-  const [form, setForm] = useState({ email: "", password: "" });
+  const [form, setForm] = useState({ email: "", password: "" })
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setForm({ ...form, [name]: value });
   };
 
-  const handleSubmit = async () => {
-    try {
-      const response = await fetch('/api/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(form),
-      });
-
-      if (response.ok) {
-        window.location.assign('/');
-      } else {
-        console.error('Login failed');
-      }
-    } catch (error) {
-      console.error('An error occurred:', error);
-    }
-  };
-
   return (
-    <div className='flex flex-col min-h-screen bg-background'>
-      {/* Header */}
-      <div className='px-10 lg:px-55 pt-10 bg-background'>
-        {}
-      </div>
-      
-      {/* Contenido Principal */}
-      <main className="flex-grow flex items-center justify-center p-4">
-        <div className="flex flex-col items-center justify-between bg-white rounded-xl shadow-md p-10 lg:p-13 gap-2 max-w-[360px] sm:max-w-md lg:max-w-lg w-full mx-auto py-16 lg:py-7">
-          <img
-            src="/Logo-Login.png"
-            alt="Logo Vive Medellín"
-            className="w-[80px] h-[70px] mb-4"
-          />
-          <div>
-            <Text classes='font-bold'>Iniciar sesión</Text>
-          </div>
-          {}
-          <div className='flex flex-col items-center gap-4 mt-4'>
-            <TextBox
-              name='email'
-              type='email'
-              placeholder='Email'
-              value={form.email}
-              onChange={handleChange}
-              className='h-8 w-[300px] lg:w-[300px] border border-gray-400 rounded p-2'
-            />
-            <TextBox
-              name='password'
-              type='password'
-              placeholder='Contraseña'
-              value={form.password}
-              onChange={handleChange}
-              className='h-8 w-[300px] lg:w-[300px] border border-gray-400 rounded p-2'
-            />
-          </div>
-          <div className='flex flex-col items-center gap-2 mt-4'>
-            <Text classes='text-sky-600 hover:cursor-pointer my-2'>Olvidé mi contraseña</Text>
-            {}
-            <Button onClick={handleSubmit}>Iniciar sesión</Button>
-          </div>
+    <div className='flex flex-col min-h-screen justify-between bg-background'>
+        <div className='px-10 lg:px-55 pt-10 bg-background'>
+            <div className="flex flex-row items-center justify-between bg-background">
+                <Icon icon='mingcute:menu-fill' classes='self-start mt-4 hover:text-gray-500 hover:cursor-pointer'></Icon>
+                <NavTitle
+                iconName='line-md:arrow-left'
+                titleClasses='text-black hover:text-gray-500 leading-none inline-block'
+                title='Regresar'
+                link= '/'
+                classes='flex flex-row gap-x-1 hover:cursor-pointer hover:border-b'
+                />
+            </div>
         </div>
-      </main>
-
-      {/* Footer */}
-      <footer className="w-full">
-        <Footer />
-      </footer>
+        <div className="flex justify-center bg-background lg:px-70 lg:w-full 2xl:px-230">
+            <div className="flex flex-col items-center justify-between bg-white rounded-xl shadow-md p-10 lg:p-13 gap-2 max-w-[360px] sm:max-w-md lg:max-w-lg w-full mx-auto py-16 lg:py-7">
+                <img
+                src="/Logo-Login.png"
+                alt="Logo Vive Medellín"
+                className="w-[80px] h-[70px] mb-4"
+                style={{ imageRendering: "auto" }}
+                />
+                <div>
+                    <Text classes='font-bold'>Iniciar sesión</Text>
+                </div>
+                <div className='flex flex-row gap-x-1 justify-start'>
+                    <Text classes='text-gray-500'>¿No tienes una cuenta?</Text>
+                    <NavTitle
+                    title='Registrarse'
+                    link='/Register'
+                    classes='text-sky-600'
+                    />
+                </div>
+                <form className='flex flex-col items-center gap-4'>
+                    <TextBox
+                    name='email'
+                    type='email'
+                    placeholder='Email'
+                    value={form.email}
+                    onChange={handleChange}
+                    className='h-8 w-[300px] lg:w-[300px] border border-gray-400 rounded p-2'
+                    />
+                    <TextBox
+                    name='password'
+                    type='password'
+                    placeholder='Contraseña'
+                    value={form.password}
+                    onChange={handleChange}
+                    className='h-8 w-[300px] lg:w-[300px] border border-gray-400 rounded p-2'
+                    />
+                </form>
+                <div className='flex flex-col items-center gap-2'>
+                    <Text classes='text-sky-600 hover:cursor-pointer my-2'>Olvidé mi contraseña</Text>
+                    <Button>Iniciar sesión</Button>
+                </div>
+            </div>
+        </div>
+        <footer className="w-full">
+            <Footer />
+        </footer>
     </div>
   )
 }
--- src/app/api/register/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-
-// Esta función se ejecutará cuando el frontend haga un POST a /api/register
-export async function POST(request: Request) {
-  console.log('API /api/register fue llamada');
-  return NextResponse.json({ message: 'User created successfully' }, { status: 201 });
-}
